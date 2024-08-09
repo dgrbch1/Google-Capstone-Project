3d04a390890e187c5d6ab25e846fcd5cd0720a6a
dgrbch1 <wqweqweas@gmail.com>
2024-08-09 12:33:56 -0400

    Add files via upload

---
+++ Lebron 3 best points total stats.sql
@@ -0,0 +1,13 @@
+SELECT 
+    team, 
+    season,
+    SUM(PTS) AS total_points
+
+FROM 
+    `organic-spirit-431919-e4.lebron_james_stats.lebron_james`
+GROUP BY 
+    team, 
+    season
+ORDER BY 
+    total_points DESC
+LIMIT 3;
